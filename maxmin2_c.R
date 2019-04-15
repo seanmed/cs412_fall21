a82718d75b52f7caed1aaedab919857a829874dd
joeguinness <joeguinness@gmail.com>
2019-04-15 15:08:22 -0400

    info for constructing NNlist

--- maxmin2_c.R
+++ maxmin2_c.R
@@ -12,7 +12,7 @@ x2 <- (1:nvec[2])/nvec[2]
 locs <- as.matrix(expand.grid(x1,x2))
 
 # k is the number you want to group by for max-man-2. Should divide n.
-k = 100
+k = 10
 ngroups <- n/k
 maxit <- 100
 km <- kmeans(locs, ngroups, iter.max = maxit, algorithm = "Lloyd" )
@@ -45,7 +45,7 @@ colvec <- sample(color,c,replace = T)
 plot(locs[,1], locs[,2], col = colvec[km$cluster],pch=16,cex=1)
 
 # Set covariance parameters and generate data.
-covparms <- c(variance = 4, range = 0.1, smoothness = 1.0, nugget = 0)
+covparms <- c(variance = 4, range = 0.1, smoothness = 0.5, nugget = 0)
 y=fast_Gp_sim(covparms, "matern_isotropic",locs,40)
 
 # An ordering using max-min.
@@ -59,7 +59,7 @@ locsord1 <- locs[ord1,]
 yord1 <- y[ord1]
 
 # find the ordered m nearest neighbors.
-m <- 10
+m <- 30
 NNarray = find_ordered_nn(locsord,m)
 NNarray1 = find_ordered_nn(locsord1,m)
 
@@ -68,6 +68,8 @@ NNarray1 = find_ordered_nn(locsord1,m)
 # Ungrouped
 print( system.time( ll <- vecchia_loglik(covparms, "matern_isotropic", rep(0,n), locsord, NNarray)))
 print( system.time( ll1 <- vecchia_loglik(covparms, "matern_isotropic", rep(0,n), locsord1, NNarray1)))
+print( ll - ll1 )
+
 
 NNlist <- group_obs(NNarray, 2)
 NNlist1 <- group_obs(NNarray1, 2)
@@ -75,3 +77,45 @@ NNlist1 <- group_obs(NNarray1, 2)
 # Grouped
 print( system.time(llg <- vecchia_loglik_grouped(covparms, "matern_isotropic", rep(0,n), locsord, NNlist)))
 print( system.time(llg1 <- vecchia_loglik_grouped(covparms, "matern_isotropic", rep(0,n), locsord1, NNlist1)))
+
+
+names(NNlist)
+
+# list of all indices in U_1, U_2, ...
+# listed out in order, one after the other
+NNlist$all_inds
+
+# information about the end of each U_i codified in last_ind_of_block
+NNlist$last_ind_of_block[1]
+# better name:
+# NNlist$last_ind_of_U[12]
+
+
+U12 <- NNlist$all_inds[ (NNlist$last_ind_of_block[11]+1):NNlist$last_ind_of_block[12] ]
+
+# which of these are from B12
+
+# all indicies in B1, B2, ...
+# listed out in order, one after the other
+NNlist$global_resp_inds
+
+# this tells you where they end
+NNlist$last_resp_of_block[12]
+# better name
+# last_ind_of_B[12]
+
+B12 <- NNlist$global_resp_inds[ (NNlist$last_resp_of_block[11]+1):NNlist$last_resp_of_block[12] ]
+
+
+B12 <- NNlist$global_resp_inds[ (NNlist$last_resp_of_block[11]+1):NNlist$last_resp_of_block[12] ]
+
+sv <- NNlist$local_resp_inds[ (NNlist$last_resp_of_block[11]+1):NNlist$last_resp_of_block[12] ]
+U12[sv]
+B12
+
+
+# construct global resp inds
+# look in NNarray and grab union of all neighbors for each B
+# assign to all_inds
+# ...
+
